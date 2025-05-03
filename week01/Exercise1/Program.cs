2e8f80c99a94e670e082adedadeedf3ab2561972
MarcosSilvera <ma.silveram@gmail.com>
2025-05-03 16:28:09 -0300

    Finished C# Programming Exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -6,7 +6,7 @@ class Program
     {
         Console.Write("What is your first name? ");
         string first = Console.ReadLine();
-        Console.Write("What is your first name? ");
+        Console.Write("What is your last name? ");
         string last = Console.ReadLine();
         Console.WriteLine($"Your name is {last}, {first} {last}.");
 
