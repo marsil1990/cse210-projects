ee501c461a6c3e8f22b2c30380bac6ff505d1fe1
MarcosSilvera <ma.silveram@gmail.com>
2025-05-03 19:57:16 -0300

    Finished C# Programming Exercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWecome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        number = SquareNumber(number);
+        DisplayResult(name, number);
+    }
+
+    static void DisplayWecome(){
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName(){
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber(){
+        Console.Write("Please enter your favorite number: ");
+        int userNumber = int.Parse(Console.ReadLine());
+        return userNumber;
+    }
+
+    static int SquareNumber(int number){
+        return number*number;
+    }
+
+    static void DisplayResult(string userName, int squareNumber){
+        Console.WriteLine($"Brother {userName}, the square of your number is {squareNumber}");
     }
 }
